4797e924bfec8ba86004df4ebde9a020704ec71e
engrdavdg <splendourcoders@gmail.com>
2026-03-10 15:43:43 +0100

    add assignment for week1 exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Type Your First Name and Press Enter
+Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+//Type Your Last Name and Press Enter
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+//Print the user details
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
     }
 }
